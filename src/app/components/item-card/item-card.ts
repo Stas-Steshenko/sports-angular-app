587dcea3e4b7e9a34369b6d23c17972451a81702
Steshenko-Stanislav <steshenko.stanislav@student.sumdu.edu.ua>
2025-12-25 22:53:27 +0200

    Implemented data binding and dynamic styles

--- src/app/components/item-card/item-card.ts
+++ src/app/components/item-card/item-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Match } from '../../shared/models/match.model';
 
 @Component({
   selector: 'app-item-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './item-card.html',
-  styleUrl: './item-card.css',
+  styleUrl: './item-card.css'
 })
-export class ItemCard {
-
+export class ItemCardComponent {
+  @Input() match!: Match;
 }
